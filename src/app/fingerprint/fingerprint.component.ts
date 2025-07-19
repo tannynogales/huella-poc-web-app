515d7edad7ebecd9eb1816874441c1425c15ce3c
Tanny Nogales Beltran <tanny.nogales@gmail.com>
2025-07-19 00:58:03 -0400

    add errores

--- src/app/fingerprint/fingerprint.component.ts
+++ src/app/fingerprint/fingerprint.component.ts
@@ -7,24 +7,33 @@ import { Component, OnDestroy, OnInit, signal } from '@angular/core';
   styleUrl: './fingerprint.component.scss',
 })
 export class FingerprintComponent implements OnInit, OnDestroy {
-  private ws!: WebSocket;
-  fingerprint = signal<string | null>(null);
+ fingerprint = signal<string | null>(null);
   error = signal<string | null>(null);
+  private ws!: WebSocket;
 
   ngOnInit() {
+    console.log('Conectando al WebSocket...');
     this.ws = new WebSocket('ws://localhost:4001');
+
+    this.ws.onopen = () => console.log('WebSocket abierto');
     this.ws.onmessage = (msg) => {
-      const { event, data, message } = JSON.parse(msg.data);
-      if (event === 'finger-captured') {
-        this.fingerprint.set(data);
-      } else if (event === 'error') {
-        this.error.set(message);
+      console.log('Mensaje recibido:', msg.data);
+      const parsed = JSON.parse(msg.data);
+      if (parsed.event === 'finger-captured') {
+        this.fingerprint.set(parsed.data);
+      } else if (parsed.event === 'error') {
+        this.error.set(parsed.message);
       }
     };
-    this.ws.onerror = () => this.error.set('WebSocket connection error');
+
+    this.ws.onerror = (err) => {
+      console.error('Error WebSocket:', err);
+      this.error.set('Error en la conexión WebSocket');
+    };
   }
 
   ngOnDestroy() {
-    this.ws.close();
-  }
+    console.log('Cerrando WebSocket');
+     if (this.ws) this.ws.close();
+   }
 }
